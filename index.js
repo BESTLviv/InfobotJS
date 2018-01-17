a8a33b6d31570f3faadf65bb2acc5d2051e43978
Your Grandma <dmytro.chalyi.corp@gmail.com>
2018-01-17 13:11:01 +0200

    fixed or not?

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 import TelegramBot from 'node-telegram-bot-api'
-import config from 'config'
 
 const token =TOKEN;
 const port = process.env.PORT || 443;
 const host = process.env.HOST;
 const externalUrl = process.env.MYURL;
 let bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+
 bot.setWebHook(externalUrl + ':443/bot' + token);
-
-//const app = new Koa();
-//const router = Router();
-//app.listen
-
-// router.post('/bot', ctx=>{
-//     console.log(ctx);
-//     ctx.status = 200;
-// });
-//
-// app.use(router.routes());
 
 bot.on('message', msg=> {
     const {chat: {id}}=msg;
