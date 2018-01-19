b66a9a7f154dca8a22cff9439df433e14baa9944
Your Grandma <dmytro.chalyi.corp@gmail.com>
2018-01-19 20:14:57 +0200

    newtest

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var TelegramBot = require( 'node-telegram-bot-api');
 
-var token = process.env.TOKEN;
+var token = process.env.TG_TOKEN;
 var port = process.env.PORT || 443;
 var host = process.env.HOST;
 var externalUrl = process.env.MYURL;
-var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+var bot = new TelegramBot(token);
 
-bot.setWebHook(externalUrl + ':443/bot' + token);
+bot.setWebHook(externalUrl + token);
 
 bot.on('message', function (msg) {
     var chatId=msg.chatId.id;
