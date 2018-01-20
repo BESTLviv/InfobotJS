085dd4524f1d0ac2c87230ac113b3ea9720bb862
Your Grandma <dmytro.chalyi.corp@gmail.com>
2018-01-20 09:53:44 +0200

    data test

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+/*
+in json:
+1 - last name
+2 - first name
+3 - vkontakte
+4 - facebook
+5 - b-day
+6 - phone
+7 - email
+8 - status
+9 - member since
+10 - angel
+
+*/
+
 var TelegramBot = require( 'node-telegram-bot-api');
+var fs = require("fs");
 
 var token = process.env.TG_TOKEN;
 var port = process.env.PORT || 443;
@@ -6,9 +22,15 @@ var host = process.env.HOST;
 var externalUrl = process.env.MYURL;
 var bot = new TelegramBot(token, {webHook:{port:process.env.PORT}});
 
+
 bot.setWebHook(externalUrl +':443/bot' +token);
+console.log('Webhook has been set.')
+
+var content = fs.readFileSync("content.txt");
+console.log("lbg info : \n"+ content);
+console.log("\n info is loaded \n");
 
 bot.on('message', function (msg) {
     var chatId=msg.from.id;
-    bot.sendMessage(chatId, 'Pong');
+    bot.sendMessage(chatId, msg.Text);
 });
