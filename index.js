eb13b5d938751cdd1615091e39161c39f67d24fd
Your Grandma <dmytro.chalyi.corp@gmail.com>
2018-01-20 10:36:33 +0200

    test

--- index.js
+++ index.js
@@ -32,11 +32,12 @@ console.log("\n info is loaded, len = "+ content.length+ " 1 obj len: "+content[
 
 
 bot.onText(/\/get/, function (msg) {
-    var info
-    for (i=0; i<content.length; i++)
-        if (content[i].includes(msg.text.ToString()) === true){
-        info = content[i][2]+"'s phone: +380"+ content[i][6]
+    var info="no user found\n"
+    for (i=0; i<content.length; i++) {
+        if (content[i].includes(msg.text.ToString()) === true) {
+            info = content[i][2] + "'s phone: +380" + content[i][6] + "\n"
         }
+    }
     var chatId=msg.from.id;
     bot.sendMessage(chatId, msg.text.ToString());
 });
