a24ff50b2edf4f23238699d8e3b1afd6267e4ab6
Your Grandma <dmytro.chalyi.corp@gmail.com>
2018-01-20 10:50:29 +0200

    shoud read doc but not

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ console.log("lbg info : \n"+ content[1]);
 console.log("\n info is loaded, len = "+ content.length+ " 1 obj len: "+content[1].length+ "/n");
 
 
-bot.onText(/\/help/, function (msg) {
+bot.onText(/\/help (.+)/, function (msg, match) {
     var info="no user found\n"
     for (i=0; i<content.length; i++) {
-        if (content[i].includes(msg.text.ToString()) === true) {
+        if (content[i].includes(match[1]) === true) {
             info = content[i][2] + "'s phone: +380" + content[i][6] + "\n"
         }
     }
